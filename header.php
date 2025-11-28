93f44d8213ca8331df2851040eb77543cf95c900
rt656arch <rt656@drexel.edu>
2025-11-28 01:17:22 -0500

    Initial commit of existing content

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>IDM232 RECIPE WEBSITE</title>
+</head>
+<body>  
+    <nav class="navbar">
+        <div class="navbar-container">
+            <a href="index.html" class="navbar-logo">Recipes</a>
+            <button class="navbar-toggle">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </button>
+            <ul class="navbar-menu">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="help.html">Help</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class="search-bar">
+            <input type="text" placeholder="Search...">
+            <button type="submit"></button>
+    </div>
---
+++ index.php
@@ -0,0 +1,92 @@
+<?php
+include 'header.php';
+require_once 'config.php';
+
+$conn = getDBConnection();
+?>
+
+ <section class="title">
+        <div class="titleText">
+            <h1>DISCOVER NEW AMAZING RECIPES</h1>
+            <p>For your homecooked meals</p>
+        </div>
+        <div class="titleImg">
+            <img src="images/title image.png" alt="Cartoon title image">
+        </div>
+    </section>
+
+    <section class="home-recipes">
+        <h2>Newly Added Recipes</h2>
+            <div class="newRecipeContainer">
+                <div class="recipeCard">
+                    <div class="recipeImg">
+                        <a href="single-recipe.html" target="_blank"> <img src="images/0101_FPP_Chicken-Rice_97338_WEB_SQ_hi_res.jpg" alt="Chicken Rice"></a>
+                    </div>
+                    <div class="recipeName">
+                        <h3>Ancho-Orange Chicken</h3>
+                        <div class="recipeName2">
+                            <p>with Kale Rice & Roasted Carrots</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="recipeCard">
+                    <div class="recipeImg">
+                        <a href="single-recipe.html" target="_blank"> <img src="images/0101_FPP_Chicken-Rice_97338_WEB_SQ_hi_res.jpg" alt="Chicken Rice"></a>
+                    </div>
+                    <div class="recipeName">
+                        <h3>Ancho-Orange Chicken</h3>
+                        <div class="recipeName2">
+                            <p>with Kale Rice & Roasted Carrots</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="recipeCard">
+                    <div class="recipeImg">
+                        <a href="single-recipe.html" target="_blank"> <img src="images/0101_FPP_Chicken-Rice_97338_WEB_SQ_hi_res.jpg" alt="Chicken Rice"></a>
+                    </div>
+                    <div class="recipeName">
+                        <h3>Ancho-Orange Chicken</h3>
+                        <div class="recipeName2">
+                            <p>with Kale Rice & Roasted Carrots</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="recipeCard">
+                    <div class="recipeImg">
+                        <a href="single-recipe.html" target="_blank"> <img src="images/0101_FPP_Chicken-Rice_97338_WEB_SQ_hi_res.jpg" alt="Chicken Rice"></a>
+                    </div>
+                    <div class="recipeName">
+                        <h3>Ancho-Orange Chicken</h3>
+                        <div class="recipeName2">
+                            <p>with Kale Rice & Roasted Carrots</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="recipeCard">
+                    <div class="recipeImg">
+                        <a href="single-recipe.html" target="_blank"> <img src="images/0101_FPP_Chicken-Rice_97338_WEB_SQ_hi_res.jpg" alt="Chicken Rice"></a>
+                    </div>
+                    <div class="recipeName">
+                        <h3>Ancho-Orange Chicken</h3>
+                        <div class="recipeName2">
+                            <p>with Kale Rice & Roasted Carrots</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="recipeCard">
+                    <div class="recipeImg">
+                        <a href="single-recipe.html" target="_blank"> <img src="images/0101_FPP_Chicken-Rice_97338_WEB_SQ_hi_res.jpg" alt="Chicken Rice"></a>
+                    </div>
+                    <div class="recipeName">
+                        <h3>Ancho-Orange Chicken</h3>
+                        <div class="recipeName2">
+                            <p>with Kale Rice & Roasted Carrots</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>
+
+    <?php
+    include 'footer.php';
+    ?>
